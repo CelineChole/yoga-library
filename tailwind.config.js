95309edc7291715f1fcea4ef3e8c6305f7711146
celinechole <github@celinechole.com>
2019-09-04 17:55:02 -0400

    install tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          "accent-1": "#f3f9fb",
+          "accent-2": "#87c0cd",
+          "accent-3": "#226597",
+          "accent-4": "#113f67"
+        },
+      },
+    },
+    variants: {},
+    plugins: [],
+  }
