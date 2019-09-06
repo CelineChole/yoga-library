bd4022ac82f736a119165eec413e3f711204a4aa
celinechole <github@celinechole.com>
2019-09-05 22:00:34 -0400

    update accents

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,11 @@ module.exports = {
     theme: {
       extend: {
         colors: {
-          "accent-1": "#f3f9fb",
-          "accent-2": "#87c0cd",
-          "accent-3": "#226597",
-          "accent-4": "#113f67"
+          "accent-1": "#422A26",
+          "accent-2": "#9B6A6C",
+          "accent-3": "#B09398",
+          "accent-4": "#CEDFD9",
+          "accent-5": "EBFCFB",
         },
       },
     },
