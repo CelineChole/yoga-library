fcf76eb3ee9f13a70f51da292864fd28ca1ec5e1
celinechole <github@celinechole.com>
2019-09-06 09:00:39 -0400

    update layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const IndexPage = props => {
       <SEO title="Home" />
 
       <div className="min-h-screen flex flex-col">
-        <header className="bg-accent-2 border-accent-2 border-b p-4 mb-6">
+        <header className="bg-accent-3 border-accent-2 border-b p-4 mb-6">
           <h1>
             <Link
               className="text-2xl font-extrabold hover:text-accent-4"
@@ -23,12 +23,12 @@ const IndexPage = props => {
             </Link>
           </h1>
         </header>
-        <main className="flex flex-wrap max-w-5xl px-4 md:px-8 mx-auto">
+        <main className="flex -mx-1 flex-wrap max-w-5xl px-4 md:px-8">
           {videos.map(video => {
             return (
                 <div
                   key={video.poseid}
-                  className="w-1/3 my-2 p-4 rounded overflow-hidden hover:text-blue-700 shadow-lg"
+                  className="w-1/3 px-3 p-4 rounded overflow-hidden hover:text-blue-700 shadow-lg"
                 >
                   <a href={video.url} target="_blank">
                     <img
@@ -36,19 +36,21 @@ const IndexPage = props => {
                       src={video.thumbnail}
                       alt="Sunset in the mountains"
                     />
-                    <div className="px-6 py-4">
-                      <div className="font-bold text-xl mb-2">
+                    <div className="px-6 py-2">
+                      <div className="font-bold text-xl mb-1">
                         {video.title}
                       </div>
                     </div>
                   </a>
-                  <div className="px-6 py-4">
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-green-700 mr-2">
+                  <div className="px-6 py-1">
+                    <div className="inline-block px-3 py-1 text-xl font-semibold text-green-700 mr-2">
                       {video.level}
-                    </span>
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-red-700 mr-2">
+                    </div>
+                    <div className="inline-block px-3 py-1 text-sm font-semibold text-red-700 mr-2">
                       {video.length} min
-                    </span>
+                    </div>
+                  </div>
+                  <div className="px-6 py-1">
                     <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
                       {video.channel}
                     </span>
@@ -61,11 +63,10 @@ const IndexPage = props => {
         <footer className="bg-accent-2 border-accent-2 border-t pb-10 pt-6 mt-6 flex flex-col items-center">
           <div className="mb-4 font-semibold">
             <a className="hover:text-accent-4" href="https://github.com/CelineChole/" target="_blank">
-              Build by 
+            {/* Build and designed by {author} • {new Date().getFullYear()} • itineranturweb.com */}
             </a>
           </div>
           <div>
-            {/* {author} • {new Date().getFullYear()} • itineranturweb.com */}
           </div>
         </footer>
       </div>
