136d83b764fbd35b33ffdff936c362cbc6893128
celinechole <github@celinechole.com>
2019-09-07 11:04:35 -0400

    cards for channel

--- src/templates/channel.js
+++ src/templates/channel.js
@@ -7,23 +7,55 @@ const Channel = ({ pageContext, data }) => {
   const { nodes, totalCount } = data.allGoogleSheetMasterRow
   const channelHeader = `${totalCount} video${
     totalCount === 1 ? "" : "s"
-  } for YouTube channel "${channel}"`
+  } for "${channel}" YouTube channel `
 
   return (
     <Layout>
       <main className="px-6 md:px-8">
-        <div>
-          <h1 className="mb-4 text-xl font-bold">{channelHeader}</h1>
-          <ul>
-            {nodes.map(video => {
-              return (
-                <li key={video.title}>
-                  <a href={video.url} target="_blank">{video.title}</a>
-                </li>
-              )
-            })}
-          </ul>
-          <Link to="/channels">All Channels</Link>
+        <h1 className="mb-4 text-xl font-bold">{channelHeader}</h1>
+        <div className="flex -mx-2 flex-wrap">
+          {nodes.map(video => {
+            return (
+              <div
+                key={video.poseid}
+                className="md:w-1/4 relative lg:w-1/5 xl:w-1/8 px-3 my-4"
+              >
+                <div className="flex flex-col h-full rounded overflow-hidden hover:text-accent-3 shadow-lg hover:bg-gray-100">
+                  <a href={video.url} target="_blank">
+                    <img
+                      className="w-full"
+                      src={video.thumbnail}
+                      alt={video.title}
+                    />
+                    <div className="px-3 py-2">
+                      <div className="font-bold text-sm mb-1">
+                        {video.title}
+                      </div>
+                    </div>
+                  </a>
+                  <div className="flex flex-1 flex-col justify-end">
+                    <div className="flex-0 px-4 py-1">
+                      <div className="inline-block px-2 py-1 text-xl mr-2">
+                        {video.level}
+                      </div>
+                      <div className="inline-block px-2 py-1 text-sm font-medium text-accent-3 mr-2">
+                        <Link to={`/tag/${video.tag}`}>
+                          {video.tag}
+                        </Link>
+                      </div>
+                    </div>
+                    <div className="px-4 py-1 mb-2">
+                      <span className="inline-block bg-gray-200 rounded-full px-2 py-1 text-sm font-medium text-gray-700 hover:bg-accent-3 hover:text-white">
+                          <Link to={`/duration/${video.duration}`}>
+                          {video.duration} min
+                          </Link>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )
+          })}
         </div>
       </main>
     </Layout>
@@ -34,10 +66,15 @@ export default Channel
 
 export const pageQuery = graphql`
   query($channel: String) {
-    allGoogleSheetMasterRow(filter: {channel: {eq: $channel}}) {
+    allGoogleSheetMasterRow(filter: { channel: { eq: $channel } }) {
       nodes {
         title
         url
+        tag
+        thumbnail
+        duration
+        level
+        poseid
       }
       totalCount
     }
