16dada582dbf46f1767ef1f8f96426569d2ff387
celinechole <github@celinechole.com>
2019-09-08 16:19:33 -0400

    update config name

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `yogi-library`,
+        short_name: `Yogi Library`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
