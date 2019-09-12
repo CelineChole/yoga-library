2980f2f0e39d4c10710a2f8cf750f992ef6154b8
celinechole <github@celinechole.com>
2019-09-12 11:22:53 -0400

    remove unsued import

--- src/templates/channel.js
+++ src/templates/channel.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react"
 import Layout from "../components/layout"
-import { Link, graphql } from "gatsby"
-import DisplayLevel from "../components/displayLevel"
+import { graphql } from "gatsby"
 import { SortFilters } from "../components/SortFilters";
 import sortList from "../utilities/sortList"
+import { VideoCard } from "../components/VideoCard"
 
 const Channel = ({ pageContext, data }) => {
   const { channel } = pageContext
@@ -28,44 +28,7 @@ const Channel = ({ pageContext, data }) => {
         <div className="flex -mx-3 flex-wrap">
           {sortedList.map(video => {
             return (
-              <div
-                key={video.poseid}
-                className="md:w-1/4 relative lg:w-1/5 xl:w-1/8 px-3 my-4"
-              >
-                <div className="flex flex-col h-full rounded overflow-hidden hover:text-accent-3 shadow-lg hover:bg-gray-100">
-                  <a href={video.url} target="_blank">
-                    <img
-                      className="w-full"
-                      src={video.thumbnail}
-                      alt={video.title}
-                    />
-                    <div className="px-3 py-2">
-                      <div className="font-bold text-sm mb-1">
-                        {video.title}
-                      </div>
-                    </div>
-                  </a>
-                  <div className="flex flex-1 flex-col justify-end">
-                    <div className="flex-0 px-4 py-1">
-                      <div className="inline-block px-2 py-1 text-xl mr-2">
-                      <Link to={`/level/${video.level}`}>
-                        <DisplayLevel level={video.level} />
-                      </Link>
-                      </div>
-                      <span className="inline-block bg-gray-200 rounded px-2 py-1 text-sm font-medium text-gray-700 hover:bg-accent-3 hover:text-white">
-                        <Link to={`/duration/${video.duration}`}>
-                          {video.duration} min
-                        </Link>
-                      </span>
-                    </div>
-                    <div className="px-4 py-1 mb-2">
-                      <div className="inline-block px-2 py-1 text-sm font-medium text-accent-3 mr-2">
-                        <Link to={`/tag/${video.tag}`}>{video.tag}</Link>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <VideoCard key={video.poseId} video={video} hidden="channels" />
             )
           })}
         </div>
