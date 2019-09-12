90d362537bf3d497812ef912686e5aef30e9af7e
celinechole <github@celinechole.com>
2019-09-12 19:35:54 -0400

    card display on parent component

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,8 @@ import sortList from "../utilities/sortList"
 import SEO from "../components/seo"
 
 import { graphql } from "gatsby"
-import { SortFilters } from "../components/SortFilters";
-import { VideoCardLarge } from "../components/VideoCardLarge"
+import { SortFilters } from "../components/SortFilters"
+import { VideoCard } from "../components/VideoCard"
 
 const IndexPage = props => {
   const videos = props.data.allGoogleSheetMasterRow.nodes
@@ -21,11 +21,21 @@ const IndexPage = props => {
       <SEO title="Home" />
       <Layout>
         <div className="flex flex-col">
-          <SortFilters sort={sort} setSort={setSort} sortDescending={sortDescending} setSortDescending={setSortDescending} />
+          <SortFilters
+            sort={sort}
+            setSort={setSort}
+            sortDescending={sortDescending}
+            setSortDescending={setSortDescending}
+          />
           <div className="flex flex-wrap px-6 md:px-8">
             {sortedList.map(video => {
               return (
-                <VideoCardLarge key={video.poseid} video={video} />
+                <div
+                  key={video.poseid}
+                  className="md:w-1/2 relative lg:w-1/3 px-3 my-4"
+                >
+                  <VideoCard video={video} />
+                </div>
               )
             })}
           </div>
@@ -54,4 +64,3 @@ export const data = graphql`
     }
   }
 `
-
