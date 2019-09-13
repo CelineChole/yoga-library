39782136314cf689deda98047b8a734cce81849c
celinechole <github@celinechole.com>
2019-09-12 20:05:43 -0400

    refactor tag with video card

--- src/templates/tag.js
+++ src/templates/tag.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import DisplayLevel from "../components/displayLevel"
 import { Link, graphql } from "gatsby"
+import { VideoCard } from "../components/VideoCard"
 
 const Tag = ({ pageContext, data }) => {
   const { tag } = pageContext
@@ -18,45 +19,11 @@ const Tag = ({ pageContext, data }) => {
           {nodes.map(video => {
             return (
               <div
-                key={video.poseid}
-                className="md:w-1/4 relative lg:w-1/5 xl:w-1/8 px-3 my-4"
-              >
-                <div className="flex flex-col h-full rounded overflow-hidden hover:text-accent-3 shadow-lg hover:bg-gray-100">
-                  <a href={video.url} target="_blank">
-                    <img
-                      className="w-full"
-                      src={video.thumbnail}
-                      alt={video.title}
-                    />
-                    <div className="px-3 py-2">
-                      <div className="font-bold text-sm mb-1">
-                        {video.title}
-                      </div>
-                    </div>
-                  </a>
-                  <div className="flex flex-1 flex-col justify-end">
-                    <div className="flex-0 px-4 py-1">
-                      <div className="inline-block px-2 py-1 text-xl mr-2">
-                        <Link to={`/level/${video.level}`}>
-                          <DisplayLevel level={video.level} />
-                        </Link>
-                      </div>
-                      <div className="inline-block px-2 py-1 text-sm font-medium text-accent-3 mr-2">
-                        <Link to={`/duration/${video.duration}`}>
-                          {video.duration} min
-                        </Link>
-                      </div>
-                    </div>
-                    <div className="px-4 py-1 mb-2">
-                      <span className="inline-block bg-gray-200 rounded-full px-2 py-1 text-sm font-medium text-gray-700 hover:bg-accent-3 hover:text-white">
-                        <Link to={`/channel/${video.channel}`}>
-                          {video.channel}
-                        </Link>
-                      </span>
-                    </div>
-                  </div>
+                  key={video.poseid}
+                  className="flex flex-wrap md:w-1/4 xl:w-1/8 px-2 my-4 md:px-3"
+                >
+                  <VideoCard video={video} hidden="tag" />
                 </div>
-              </div>
             )
           })}
         </div>
