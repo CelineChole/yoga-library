891f578d198df4a49c4a383f5debf8be9f0a9b84
celinechole <github@celinechole.com>
2019-09-12 20:08:05 -0400

    implement sort logic

--- src/templates/tag.js
+++ src/templates/tag.js
@@ -1,7 +1,8 @@
-import React from "react"
+import React, { useState } from "react"
 import Layout from "../components/layout"
-import DisplayLevel from "../components/displayLevel"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
+import { SortFilters } from "../components/SortFilters"
+import sortList from "../utilities/sortList"
 import { VideoCard } from "../components/VideoCard"
 
 const Tag = ({ pageContext, data }) => {
@@ -11,12 +12,27 @@ const Tag = ({ pageContext, data }) => {
     totalCount === 1 ? "" : "s"
   } of ${tag}`
 
+  const videos = nodes
+
+  const [sort, setSort] = useState("level")
+  const [sortDescending, setSortDescending] = useState(true)
+
+  const sortedList = sortList(videos, sort, sortDescending)
+
   return (
     <Layout>
-      <main className="px-6 md:px-8">
+    <div className="flex flex-col">
+        <SortFilters
+          sort={sort}
+          setSort={setSort}
+          sortDescending={sortDescending}
+          setSortDescending={setSortDescending}
+          hidden=""
+        />
+      <div className="px-6 md:px-8">
         <h1 className="mb-4 text-xl font-bold">{tagHeader}</h1>
         <div className="flex -mx-2 flex-wrap">
-          {nodes.map(video => {
+          {sortedList.map(video => {
             return (
               <div
                   key={video.poseid}
@@ -27,7 +43,8 @@ const Tag = ({ pageContext, data }) => {
             )
           })}
         </div>
-      </main>
+      </div>
+      </div>
     </Layout>
   )
 }
